968c9e89701500746af71ebc2630c6aadd24da63
Максим <aaalllooollk@gmail.com>
2023-05-08 10:50:33 +0300

    added stdafx

---
+++ AppFramework/stdafx.h
@@ -0,0 +1,14 @@
+#pragma once
+#define _WIN32_WINNT_WIN10_TH2 0x295A
+#define _WIN32_WINNT_WIN10_RS1 0x3839
+#define _WIN32_WINNT_WIN10_RS2 0x3AD7
+#define _WIN32_WINNT_WIN10_RS3 0x3FAB
+#define _WIN32_WINNT_WIN10_RS4 0x42EE
+#define _WIN32_WINNT_WIN10_RS5 0x4563
+#pragma warning(disable: 5039)
+#include <Windows.h>
+#pragma warning(default: 5039)
+
+#pragma warning(disable: 4820)
+#include <d3d9.h>
+#pragma warning(default: 4820)
