4797cae7693dae1bc0054c5728c5e09dc21ecf32
celisej567 <celisej567@gmail.com>
2023-05-08 10:25:37 +0300

    aboba

--- AppFramework/main.cpp
+++ AppFramework/main.cpp
@@ -3,7 +3,6 @@
 //	purpose: existing
 //
 //===================================================================
-
 #define _WIN32_WINNT_WIN10_TH2 0x295A
 #define _WIN32_WINNT_WIN10_RS1 0x3839
 #define _WIN32_WINNT_WIN10_RS2 0x3AD7
@@ -13,7 +12,7 @@
 #pragma warning(disable: 5039)
 #include <Windows.h>
 #pragma warning(default: 5039)
-
+#include "CApp.h"
 
 LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
@@ -32,6 +31,8 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _
 {
 	HWND hWnd;
 	WNDCLASSEX wc = { 0 };
+
+	
 
 	wc.cbSize = sizeof(WNDCLASSEX);
 	wc.style = CS_HREDRAW | CS_VREDRAW;
@@ -55,16 +56,26 @@ int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _
 		NULL,
 		hInstance,
 		NULL);
+	if (hWnd == nullptr)
+		return -1;
 
 	ShowWindow(hWnd, iCmdShow);
 
 	MSG msg;
 
+	CApp app;
+	app.Init(hWnd);
+
 	while (GetMessage(&msg, NULL, 0, 0))
 	{
 		TranslateMessage(&msg);
 		DispatchMessage(&msg);
+
+		app.Render();
 	}
+
+	app.Release();
+
 	return (int)msg.wParam;
 	UNREFERENCED_PARAMETER(lpCmdLine);
 	UNREFERENCED_PARAMETER(hPrevInstance);
