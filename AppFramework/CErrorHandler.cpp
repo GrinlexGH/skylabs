f7eeb96a03c5a72915b8166e9e54735cacdba361
Максим <aaalllooollk@gmail.com>
2023-05-09 16:12:48 +0300

    error handler

---
+++ AppFramework/CErrorHandler.cpp
@@ -0,0 +1,7 @@
+//=========== (C) Copyright 2023 NTT All rights reserved. ===========
+//
+// Purpose: Error handler class
+//
+//===================================================================
+#include "stdafx.h"
+#include "CErrorHandler.h"
