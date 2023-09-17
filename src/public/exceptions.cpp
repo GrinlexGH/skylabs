e6bae86f9a9a161cbba69c25db087af6a52df1a4
Максим <77984877+GrinlexGH@users.noreply.github.com>
2023-09-17 12:02:39 +0300

    done with exceptions, char converters, base app

--- src/public/exceptions.cpp
+++ src/public/exceptions.cpp
@@ -2,14 +2,22 @@
 #include "charconverters.hpp"
 #include "exceptions.hpp"
 
-localized_exception::localized_exception(const std::wstring_view msg) noexcept : message(CharConverters::WideStrToUTF8<std::u8string>(msg))
+localized_exception::localized_exception(const std::wstring_view msg) noexcept : message(CharConverters::WideStrToUTF8<std::string>(msg))
  { }
-localized_exception::localized_exception(const std::u8string_view msg) noexcept : message(msg)
+localized_exception::localized_exception(const std::u8string_view msg) noexcept : message(std::bit_cast<const char*>(msg.data()))
  { }
-localized_exception::localized_exception(const std::string_view msg) noexcept : message(std::bit_cast<const char8_t*>(msg.data()))
+localized_exception::localized_exception(const std::string_view msg) noexcept : message(msg)
  { }
 
 const char* localized_exception::what() const noexcept {
-    return std::bit_cast<const char*>(message.c_str());
+    return message.c_str();
 }
+
+
+current_func_exception::current_func_exception(const std::wstring_view msg) noexcept : message(CurrentFunction + ": " + (CharConverters::WideStrToUTF8<std::string>(msg)))
+{ }
+current_func_exception::current_func_exception(const std::u8string_view msg) noexcept : message(CurrentFunction + ": " + (std::bit_cast<const char*>(msg.data())))
+{ }
+current_func_exception::current_func_exception(const std::string_view msg) noexcept : message(CurrentFunction + ": " + std::string(msg))
+{ }
 
--- src/public/exceptions.hpp
+++ src/public/exceptions.hpp
@@ -2,11 +2,15 @@
 
 #include <exception>
 #include <string>
+#include "macros.hpp"
 
+/**
+* @brief Accepts any kind of strings and saves it in utf8.
+*/
 class localized_exception : public std::exception
 {
 private:
-    const std::u8string message = u8"Unknown localized exception!";
+    const std::string message = "Unknown localized exception!";
 public:
     localized_exception() noexcept = default;
     explicit localized_exception(const std::wstring_view msg) noexcept;
@@ -14,5 +18,19 @@ public:
     explicit localized_exception(const std::string_view msg) noexcept;
     ~localized_exception() noexcept override = default;
     const char* what() const noexcept override;
+};
+
+/**
+* @brief Inherits from localized_exception and just adds current function name + message
+* like Person::SayHello: Cant say hello because code page is invalid.
+*/
+class current_func_exception : public localized_exception
+{
+private:
+    const std::string message = CurrentFunction + ": Unknown localized exception!";
+public:
+    explicit current_func_exception(const std::wstring_view msg) noexcept;
+    explicit current_func_exception(const std::u8string_view msg) noexcept;
+    explicit current_func_exception(const std::string_view msg) noexcept;
 };
 
