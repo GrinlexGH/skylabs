fcd9c51e4b4457595d49db7033c2d36bd1319164
Максим <77984877+GrinlexGH@users.noreply.github.com>
2023-09-30 18:37:23 +0300

    fixed typo

--- src/core/main.cpp
+++ src/core/main.cpp
@@ -1,6 +1,15 @@
-#include "macros.hpp"
+//#include "macros.hpp"
+#include <Windows.h>
+#include <stdexcept>
 
-double DLLEXPORT CoreInit() {
-return 3.14;
+extern "C" __declspec(dllexport) int CoreInit(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nShowCmd) {
+    UNREFERENCED_PARAMETER(lpCmdLine);
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(hInstance);
+    UNREFERENCED_PARAMETER(nShowCmd);
+    if (hInstance != NULL)
+        throw std::runtime_error("dsds");
+    else
+        return 3;
 }
 
