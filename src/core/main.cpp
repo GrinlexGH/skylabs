bcbbcc23a050a547055b9149f27791a50686ba3b
GrinlexGH <aaalllooollk@gmail.com>
2023-10-18 16:09:39 +0300

    done with linux

--- src/core/main.cpp
+++ src/core/main.cpp
@@ -1,105 +1,19 @@
 #include "macros.hpp"
 #include <SDL3/SDL.h>
-#include <Windows.h>
 
-#include <stdexcept>
-
-const wchar_t g_szClassName[] = L"myWindowClass";
-
-// Step 4: the Window Procedure
-LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
-{
-    switch (msg)
-    {
-    case WM_CLOSE:
-        DestroyWindow(hwnd);
-        break;
-    case WM_DESTROY:
-        PostQuitMessage(0);
-        break;
-    default:
-        return DefWindowProc(hwnd, msg, wParam, lParam);
-    }
-    return 0;
-}
-
+#ifdef _WIN32
 DllExport int CoreInit(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nShowCmd) {
     UNUSED(hInstance);
     UNUSED(hPrevInstance);
     UNUSED(lpCmdLine);
     UNUSED(nShowCmd);
-    try {
-        throw std::runtime_error("Я огузок");
-
-    }
-    catch (std::runtime_error& e) {
-        MessageBoxA(NULL, e.what(), e.what(), MB_OK);
-    }
-    SDL_Window* window;
-    SDL_Init(SDL_INIT_VIDEO);
-    window = SDL_CreateWindow("An SDL3 Window", 640, 480, SDL_WINDOW_VULKAN);
-    if (window == NULL) {
-        // In the case that the window could not be made...
-        printf("Could not create window: %s\n", SDL_GetError());
-        return 1;
-    }
-    WNDCLASSEX wc;
-    HWND hwnd;
-    MSG Msg;
-
-    //Step 1: Registering the Window Class
-    wc.cbSize = sizeof(WNDCLASSEX);
-    wc.style = 0;
-    wc.lpfnWndProc = WndProc;
-    wc.cbClsExtra = 0;
-    wc.cbWndExtra = 0;
-    wc.hInstance = hInstance;
-    wc.hIcon = LoadIcon(NULL, IDI_APPLICATION);
-    wc.hCursor = LoadCursor(NULL, IDC_ARROW);
-    wc.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
-    wc.lpszMenuName = NULL;
-    wc.lpszClassName = g_szClassName;
-    wc.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
-
-    if (!RegisterClassEx(&wc))
-    {
-        MessageBox(NULL, L"Window Registration Failed!", L"Error!",
-            MB_ICONEXCLAMATION | MB_OK);
-        return 0;
-    }
-
-    // Step 2: Creating the Window
-    hwnd = CreateWindowEx(
-        WS_EX_CLIENTEDGE,
-        g_szClassName,
-        L"The title of my window",
-        WS_OVERLAPPEDWINDOW,
-        CW_USEDEFAULT, CW_USEDEFAULT, 240, 120,
-        NULL, NULL, hInstance, NULL);
-
-    if (hwnd == NULL)
-    {
-        MessageBox(NULL, L"Window Creation Failed!", L"Error!",
-            MB_ICONEXCLAMATION | MB_OK);
-        return 0;
-    }
-
-    ShowWindow(hwnd, nShowCmd);
-    UpdateWindow(hwnd);
-    SDL_Event Event;
-    while (SDL_PollEvent(&Event)) {
-        // 1. Handle Event
-        // 2. Update Everything
-        // 3. Render Frame
-    }
-    // Step 3: The Message Loop
-    while (GetMessage(&Msg, NULL, 0, 0) > 0)
-    {
-        TranslateMessage(&Msg);
-        DispatchMessage(&Msg);
-    }
-    SDL_DestroyWindow(window);
-    SDL_Quit();
-    return (int)Msg.wParam;
+    return 14;
 }
+#else
+DllExport int CoreInit(int argc, char** argv) {
+    UNUSED(argc);
+    UNUSED(argv);
+    return 15;
+}
+#endif
 
