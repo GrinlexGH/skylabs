6a68e8a1bf28badfea7c4594277985af9f8a4505
Максим Темирбеков <77984877+GrinlexGH@users.noreply.github.com>
2024-06-17 21:59:16 +0300

    added some optimizations

--- src/public/application.hpp
+++ src/public/application.hpp
@@ -7,28 +7,17 @@ class Application {
   static bool debugMode;
 
 public:
-  /**
-   * @brief Initializes BaseApplication variables
-   * @throws func_exception with windows's description of error
-   *   if GetModuleFileName failed
-   *
-   * @throws std::bad_alloc by std::filesystem::canonical(),
-   *   if memory cannot be allocated.
-   * @throws std::filesystem::filesystem_error by
-   *   std::filesystem::canonical(), in cases where there are
-   *   underlying OS API errors, and it's instantiated with the
-   *   OS error code as the error code argument.
-   */
   static void Init();
-  /**
-   * @brief Adds the given value to the PATH environment variable.
-   * @param path - The absolute path to the folder.
-   * @throws current_func_exception with error code if _wgetenv_s failed.
-   *
-   */
   static void AddLibSearchPath(const std::string_view path);
   static void *LoadLib(std::string path);
   static void switchDebugMode();
   static bool isDebugMode();
   static std::filesystem::path rootDir;
+
+protected:
+  // Singleton stuff
+  Application(const Application &) = delete;
+  Application(Application &&) = delete;
+  Application &operator=(const Application &) = delete;
+  Application &operator=(Application &&) = delete;
 };
