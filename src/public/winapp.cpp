6ceac6e8991a37257d6e39cbbe1894b26c0055c5
Max <aaalllooollk@gmail.com>
2024-09-13 16:57:37 +0300

    window object now part of CRenderer; changed source groups of visual studio; added support of changing framebuffer size; renamed vulkan_initializer to vk_initializer

--- src/public/winapp.cpp
+++ src/public/winapp.cpp
@@ -38,26 +38,6 @@ namespace resource_loader {
 
 } // resource_loader
 
-#if 0
-    #include "platform.hpp"
-    #include "unicode.hpp"
-    #include <windows.h>
-    #include <string>
-
-std::string getWinapiErrorMessage() {
-    wchar_t* errorMsg = nullptr;
-    ::FormatMessageW(
-        FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM |
-            FORMAT_MESSAGE_IGNORE_INSERTS,
-        nullptr, GetLastError(), MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
-        errorMsg, 0, nullptr
-    );
-    std::string finalMsg = narrow(errorMsg);
-    ::LocalFree(errorMsg);
-    return finalMsg;
-}
-#endif
-
 #else
     #error
 #endif
