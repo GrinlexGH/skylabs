c22de94e76711f296234baa9b3e90f6dfb159ef2
Max <aaalllooollk@gmail.com>
2024-10-13 18:24:57 +0300

    changed render in minimized window

--- src/core/SDL_Vulkan.hpp
+++ src/core/SDL_Vulkan.hpp
@@ -1,5 +1,8 @@
 #pragma once
 
+#define VULKAN_HPP_DISPATCH_LOADER_DYNAMIC 1
+#include <vulkan/vulkan.hpp>
+
 #include "SDL_vulkan.h"
 
 namespace SDL {
--- src/core/launcher.cpp
+++ src/core/launcher.cpp
@@ -1,15 +1,13 @@
 #include "vulkanapi.hpp"
 #include "SDL.hpp"
-#include "SDL_Vulkan.hpp"
 
-#include "console.hpp"
 #include "launcher.hpp"
 
 SDL::CWindow g_window;
 
 void MainLoop(CVulkanRenderer& renderer) {
     bool quit = false;
-    bool windowMinimized = false;
+    bool minimized = false;
     while (!quit) {
         SDL_Event e;
         SDL_PollEvent(&e);
@@ -20,14 +18,20 @@ void MainLoop(CVulkanRenderer& renderer) {
             case SDL_WINDOWEVENT:
                 switch (e.window.event) {
                     case SDL_WINDOWEVENT_MINIMIZED:
-                        windowMinimized = true;
+                        minimized = true;
                         break;
+
                     case SDL_WINDOWEVENT_RESTORED:
-                        windowMinimized = false;
+                        minimized = false;
                         break;
-                }
+
+                    case SDL_WINDOWEVENT_SIZE_CHANGED:
+                        renderer.m_frameBufferResized = true;
+                        break;
+                    }
+                break;
         }
-        if (!windowMinimized) {
+        if(!minimized) {
             renderer.Draw();
         }
     }
