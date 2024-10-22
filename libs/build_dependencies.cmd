3d955c82ed90e83a741a287f1aba7cf158602a56
Max <aaalllooollk@gmail.com>
2024-10-22 22:38:53 +0300

    adjust libraries compile script, added useless dockerfile for rewriting it to github actions

--- libs/build_dependencies.cmd
+++ libs/build_dependencies.cmd
@@ -1,20 +1,34 @@
 @echo off
 setlocal
 
-rem Build SDL
-cd sources\SDL
-mkdir build
-cd build
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="..\..\..\SDL3" %* .. || exit /b 1
-cmake --build . --config Release --parallel || exit /b 1
-cmake --install . --config Release || exit /b 1
+cd sources
 
-rem Build GLM
-cd ..\..\glm
-mkdir build
-cd build
-cmake -DCMAKE_BUILD_TYPE=Release -DGLM_BUILD_TESTS=OFF -DGLM_ENABLE_CXX_20=ON -DCMAKE_INSTALL_PREFIX="..\..\..\glm" %* .. || exit /b 1
-cmake --build . --config Release --parallel || exit /b 1
-cmake --install . --config Release || exit /b 1
+if not exist "..\bin\windows\SDL3" (
+    echo Compiling SDL
+    cd SDL
+    if exist build (
+        rmdir /s /q build
+    )
+    mkdir build
+    cd build
+    cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX="..\..\..\bin\windows\SDL3" %* .. || exit /b 1
+    cmake --build . --config Release --parallel || exit /b 1
+    cmake --install . --config Release || exit /b 1
+    cd ..\..\
+)
+
+if not exist "..\bin\windows\glm" (
+    echo Compiling glm
+    cd glm
+    if exist build (
+        rmdir /s /q build
+    )
+    mkdir build
+    cd build
+    cmake -DCMAKE_BUILD_TYPE=Release -DGLM_BUILD_TESTS=OFF -DGLM_ENABLE_CXX_20=ON -DCMAKE_INSTALL_PREFIX="..\..\..\bin\windows\glm" %* .. || exit /b 1
+    cmake --build . --config Release --parallel || exit /b 1
+    cmake --install . --config Release || exit /b 1
+    cd ..\..\
+)
 
 echo Done.
--- libs/build_dependencies.sh
+++ libs/build_dependencies.sh
@@ -1,18 +1,34 @@
 #!/usr/bin/bash
 set -e
 
-cd sources/SDL
-mkdir -p build
-cd build
-cmake -DCMAKE_BUILD_TYPE=Release -DSDL_TESTS=OFF -DCMAKE_INSTALL_PREFIX="../../../SDL3" $* ..
-cmake --build . --config Release --parallel
-cmake --install . --config Release
+cd sources
 
-cd ../../glm
-mkdir -p build
-cd build
-cmake -DCMAKE_BUILD_TYPE=Release -DGLM_BUILD_TESTS=OFF -DGLM_ENABLE_CXX_20=ON -DCMAKE_INSTALL_PREFIX="../../../glm" $* ..
-cmake --build . -- all
-cmake --build . -- install
+if [ ! -d "../bin/linux/SDL3" ]; then
+    echo "Compiling SDL"
+    cd SDL
+    if [ -d "build" ]; then
+        rm -rf build
+    fi
+    mkdir -p build
+    cd build
+    cmake -DCMAKE_BUILD_TYPE=Release -DSDL_TESTS=OFF -DCMAKE_INSTALL_PREFIX="../../../bin/linux/SDL3" $* ..
+    cmake --build . --config Release --parallel
+    cmake --install . --config Release
+    cd ../../
+fi
+
+if [ ! -d "../bin/linux/glm" ]; then
+    echo "Compiling glm"
+    cd glm
+    if [ -d "build" ]; then
+        rm -rf build
+    fi
+    mkdir -p build
+    cd build
+    cmake -DCMAKE_BUILD_TYPE=Release -DGLM_BUILD_TESTS=OFF -DGLM_ENABLE_CXX_20=ON -DCMAKE_INSTALL_PREFIX="../../../bin/linux/glm" $* ..
+    cmake --build . -- all
+    cmake --build . -- install
+    cd ../../
+fi
 
 echo "Done."
