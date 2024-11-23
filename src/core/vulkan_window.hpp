f174989a0a030d0e655d39ef1f7eabb864f86246
Max <aaalllooollk@gmail.com>
2024-11-23 18:35:44 +0300

    fixed deleting surface

--- src/core/vulkan_window.hpp
+++ src/core/vulkan_window.hpp
@@ -13,11 +13,11 @@ public:
     virtual std::vector<const char*> GetRequiredInstanceExtensions() = 0;
     virtual bool GetPresentationSupport(vk::Instance instance, vk::PhysicalDevice physicalDevice, uint32_t queueFamilyIndex) = 0;
     virtual void GetDrawableSize(int* w, int* h) = 0;
-    virtual void CreateSurface(vk::Instance& instance) = 0;
+    virtual void CreateSurface(vk::Instance instance) = 0;
+    virtual void DestroySurface(vk::Instance instance) = 0;
     virtual vk::SurfaceKHR GetSurface() { return m_surface; }
 
 protected:
-    vk::Instance const* m_instance = nullptr;
     vk::SurfaceKHR m_surface {};
 };
 
