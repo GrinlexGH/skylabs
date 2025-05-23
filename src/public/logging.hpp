f740e50a20f0e41c980a54f793cd804c2af4c13c
Max <aaalllooollk@gmail.com>
2025-05-23 23:30:24 +0300

    thread safe logging

--- src/public/logging.hpp
+++ src/public/logging.hpp
@@ -1,11 +1,16 @@
 #pragma once
 #include <format>
 #include <iostream>
+#include <mutex>
 
 #include "stc.hpp"
 
 namespace Log {
-enum class CLogType : std::uint8_t
+struct LogMutex {
+    inline static std::mutex m_mutex;
+};
+
+enum class LogType : std::int8_t
 {
     eDebug = 0,
     eInfo,
@@ -14,19 +19,21 @@ enum class CLogType : std::uint8_t
 };
 
 template <class... Args>
-void Log(const CLogType type, const std::format_string<Args...> fmt, Args&&... args) {
+void Log(const LogType type, const std::format_string<Args...> fmt, Args&&... args) {
+    std::lock_guard<std::mutex> lock(LogMutex::m_mutex);
+
     std::cout << '[';
     switch (type) {
-        case CLogType::eDebug: {
+        case LogType::eDebug: {
             std::cout << stc::rgb_fg(168, 228, 160) << "Debug" << stc::reset_fg << "] ";
         } break;
-        case CLogType::eInfo: {
+        case LogType::eInfo: {
             std::cout << stc::rgb_fg(114, 159, 207) << "Info" << stc::reset_fg << "] ";
         } break;
-        case CLogType::eWarning: {
+        case LogType::eWarning: {
             std::cout << stc::rgb_fg(196, 160, 0) << "Warning" << stc::reset_fg << "] ";
         } break;
-        case CLogType::eError: {
+        case LogType::eError: {
             std::cout << stc::rgb_fg(204, 0, 0) << "Error" << stc::reset_fg << "] ";
         } break;
     }
@@ -35,21 +42,21 @@ void Log(const CLogType type, const std::format_string<Args...> fmt, Args&&... a
 
 template <class... Args>
 void Debug(const std::format_string<Args...> fmt, Args&&... args) {
-    Log(CLogType::eDebug, fmt, std::forward<Args>(args)...);
+    Log(LogType::eDebug, fmt, std::forward<Args>(args)...);
 }
 
 template <class... Args>
 void Info(const std::format_string<Args...> fmt, Args&&... args) {
-    Log(CLogType::eInfo, fmt, std::forward<Args>(args)...);
+    Log(LogType::eInfo, fmt, std::forward<Args>(args)...);
 }
 
 template <class... Args>
 void Warning(const std::format_string<Args...> fmt, Args&&... args) {
-    Log(CLogType::eWarning, fmt, std::forward<Args>(args)...);
+    Log(LogType::eWarning, fmt, std::forward<Args>(args)...);
 }
 
 template <class... Args>
 void Error(const std::format_string<Args...> fmt, Args&&... args) {
-    Log(CLogType::eError, fmt, std::forward<Args>(args)...);
+    Log(LogType::eError, fmt, std::forward<Args>(args)...);
 }
 }
