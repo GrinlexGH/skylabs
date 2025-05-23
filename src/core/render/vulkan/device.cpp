776f74484f793b479249bde07c32b410ad4c70a3
Max <aaalllooollk@gmail.com>
2025-05-23 23:34:04 +0300

    removed queue class
    + queue class has been moved into device class

--- src/core/render/vulkan/device.cpp
+++ src/core/render/vulkan/device.cpp
@@ -9,95 +9,97 @@
 namespace {
 struct CQueueFamilies
 {
-    explicit CQueueFamilies(
-        const vk::Instance& instance,
-        const Vulkan::CPhysicalDevice& physicalDevice,
-        const IVulkanWindow* window
-    ) {
-        const std::vector<vk::QueueFamilyProperties>& queueFamilies = physicalDevice.GetQueueFamilies();
-        std::optional<std::uint32_t> graphicsQueueIndex;
-        std::optional<std::uint32_t> presentQueueIndex;
-        std::optional<std::uint32_t> transferQueueIndex;
-        std::optional<std::uint32_t> computeQueueIndex;
-
-        for (std::uint32_t i = 0; const auto& queue : queueFamilies) {
-            if (queue.queueCount == 0) {
-                ++i;
-                continue;
-            }
-
-            if (queue.queueFlags & vk::QueueFlagBits::eGraphics) {
-                graphicsQueueIndex = i;
-            }
-
-            if (window->CheckQueuePresentSupport(instance, physicalDevice.GetHandle(), i)) {
-                presentQueueIndex = i;
-            }
-
-            if (queue.queueFlags & vk::QueueFlagBits::eTransfer) {
-                transferQueueIndex = i;
-            }
-
-            if (queue.queueFlags & vk::QueueFlagBits::eCompute) {
-                computeQueueIndex = i;
-            }
-
-            if (graphicsQueueIndex.has_value() &&
-                presentQueueIndex.has_value() &&
-                transferQueueIndex.has_value() &&
-                computeQueueIndex.has_value()
-            ) {
-                break;
-            }
-
-            ++i;
-        }
-
-        std::vector<const char*> missingQueues;
-        missingQueues.reserve(4);
-
-        if (!graphicsQueueIndex.has_value()) {
-            missingQueues.emplace_back("graphics");
-        }
-
-        if (!presentQueueIndex.has_value()) {
-            missingQueues.emplace_back("present");
-        }
-
-        if (!transferQueueIndex.has_value()) {
-            missingQueues.emplace_back("transfer");
-        }
-
-        if (!computeQueueIndex.has_value()) {
-            missingQueues.emplace_back("compute");
-        }
-
-        if (!missingQueues.empty()) {
-            std::stringstream error;
-            error << "System doesn't have required vulkan queue families: [";
-
-            for (std::size_t i = 0; i < missingQueues.size(); ++i) {
-                error << missingQueues[i];
-                if (i < missingQueues.size() - 1) {
-                    error << " | ";
-                }
-            }
-            error << "]!";
-
-            throw std::runtime_error(error.str());
-        }
-
-        m_graphics = *graphicsQueueIndex;   //-V1007
-        m_present = *presentQueueIndex;     //-V1007
-        m_transfer = *transferQueueIndex;   //-V1007
-        m_compute = *computeQueueIndex;     //-V1007
-    }
-
     std::uint32_t m_graphics;
     std::uint32_t m_present;
     std::uint32_t m_transfer;
     std::uint32_t m_compute;
 };
+
+CQueueFamilies GetQueueFamilies(
+    const vk::Instance& instance,
+    const Vulkan::CPhysicalDevice& physicalDevice,
+    const IVulkanWindow* window
+) {
+    const std::vector<vk::QueueFamilyProperties>& queueFamilies = physicalDevice.GetQueueFamilies();
+    std::optional<std::uint32_t> graphicsQueueIndex;
+    std::optional<std::uint32_t> presentQueueIndex;
+    std::optional<std::uint32_t> transferQueueIndex;
+    std::optional<std::uint32_t> computeQueueIndex;
+
+    for (std::uint32_t i = 0; const auto & queue : queueFamilies) {
+        if (queue.queueCount == 0) {
+            ++i;
+            continue;
+        }
+
+        if (queue.queueFlags & vk::QueueFlagBits::eGraphics) {
+            graphicsQueueIndex.emplace(i);
+        }
+
+        if (window->CheckQueuePresentSupport(instance, physicalDevice.GetHandle(), i)) {
+            presentQueueIndex.emplace(i);
+        }
+
+        if (queue.queueFlags & vk::QueueFlagBits::eTransfer) {
+            transferQueueIndex.emplace(i);
+        }
+
+        if (queue.queueFlags & vk::QueueFlagBits::eCompute) {
+            computeQueueIndex.emplace(i);
+        }
+
+        if (graphicsQueueIndex.has_value() &&
+            presentQueueIndex.has_value() &&
+            transferQueueIndex.has_value() &&
+            computeQueueIndex.has_value()
+            ) {
+            break;
+        }
+
+        ++i;
+    }
+
+    std::vector<const char*> missingQueues;
+    missingQueues.reserve(4);
+
+    if (!graphicsQueueIndex.has_value()) {
+        missingQueues.emplace_back("graphics");
+    }
+
+    if (!presentQueueIndex.has_value()) {
+        missingQueues.emplace_back("present");
+    }
+
+    if (!transferQueueIndex.has_value()) {
+        missingQueues.emplace_back("transfer");
+    }
+
+    if (!computeQueueIndex.has_value()) {
+        missingQueues.emplace_back("compute");
+    }
+
+    if (!missingQueues.empty()) {
+        std::stringstream error;
+        error << "System doesn't have required vulkan queue families: [";
+
+        for (std::size_t i = 0; i < missingQueues.size(); ++i) {
+            error << missingQueues[i];
+            if (i < missingQueues.size() - 1) {
+                error << " | ";
+            }
+        }
+        error << "]!";
+
+        throw std::runtime_error(error.str());
+    }
+
+    return {
+        .m_graphics = *graphicsQueueIndex,  //-V1007
+        .m_present = *presentQueueIndex,    //-V1007
+        .m_transfer = *transferQueueIndex,  //-V1007
+        .m_compute = *computeQueueIndex     //-V1007
+    };
+}
 
 bool EnableExtension(
     const std::vector<vk::ExtensionProperties>& availableExtensions,
@@ -125,13 +127,18 @@ CDevice::CDevice(
 ) {
     std::vector<vk::DeviceQueueCreateInfo> queueCreateInfos;
 
-    CQueueFamilies queueIndices(instance.GetHandle(), physicalDevice, window);
+    auto [
+        graphicsQueueFamily,
+        presentQueueFamily,
+        transferQueueFamily,
+        computeQueueFamily
+    ] = GetQueueFamilies(instance.GetHandle(), physicalDevice, window);
 
     std::set uniqueQueueFamilies {
-        queueIndices.m_graphics,
-        queueIndices.m_present,
-        queueIndices.m_transfer,
-        queueIndices.m_compute
+        graphicsQueueFamily,
+        presentQueueFamily,
+        transferQueueFamily,
+        computeQueueFamily
     };
 
     float queuePriority = 0.5f;
@@ -157,7 +164,7 @@ CDevice::CDevice(
 
     if (!missingExtensions.empty()) {
         std::ostringstream error;
-        error << "System doesn't have required instance extensions:\n";
+        error << "System doesn't have required device extensions:\n";
         for (const auto name : missingExtensions) {
             error << '\t' << name << '\n';
         }
@@ -184,10 +191,10 @@ CDevice::CDevice(
 
     m_handle = physicalDevice.GetHandle().createDevice(createInfo);
 
-    m_graphicsQueue = std::make_unique<CQueue>(m_handle, queueIndices.m_graphics);
-    m_presentQueue = std::make_unique<CQueue>(m_handle, queueIndices.m_present);
-    m_transferQueue = std::make_unique<CQueue>(m_handle, queueIndices.m_transfer);
-    m_computeQueue = std::make_unique<CQueue>(m_handle, queueIndices.m_compute);
+    m_graphicsQueue = { .m_handle = m_handle.getQueue(graphicsQueueFamily, 0), .m_familyIndex = graphicsQueueFamily };
+    m_presentQueue = { .m_handle = m_handle.getQueue(presentQueueFamily, 0), .m_familyIndex = presentQueueFamily };
+    m_transferQueue = { .m_handle = m_handle.getQueue(transferQueueFamily, 0), .m_familyIndex = transferQueueFamily };
+    m_computeQueue = { .m_handle = m_handle.getQueue(computeQueueFamily, 0), .m_familyIndex = computeQueueFamily };
 }
 
 CDevice::~CDevice() {
--- src/core/render/vulkan/queue.cpp
+++
@@ -1,9 +0,0 @@
-#include "queue.hpp"
-
-namespace Vulkan {
-CQueue::CQueue(const vk::Device& device, const std::uint32_t familyIndex) :
-    m_familyIndex(familyIndex)
-{
-    m_handle = device.getQueue(familyIndex, 0);
-}
-}
