5311e9b82afc8767f0654169ac55132791d2a7ca
Max <aaalllooollk@gmail.com>
2025-11-05 07:37:09 +0300

    Add pipeline

--- skylabs/core/render/vulkan/pipeline.hpp
+++ skylabs/core/render/vulkan/pipeline.hpp
@@ -5,7 +5,15 @@ namespace Vulkan {
 class CPipeline
 {
 public:
-    explicit CPipeline(const CContext& context);
+    explicit CPipeline(std::nullptr_t) {}
+    explicit CPipeline(
+        const CContext& context,
+        std::span<const vk::PipelineShaderStageCreateInfo> shaderStages,
+        std::span<const vk::DescriptorSetLayout> descriptorSetLayouts,
+        vk::VertexInputBindingDescription vertexBindingDescription,
+        std::span<const vk::VertexInputAttributeDescription> vertexDescription,
+        vk::RenderPass renderPass = nullptr
+    );
     CPipeline(const CPipeline&) = delete;
     CPipeline(CPipeline&&) noexcept = default;
     CPipeline& operator=(const CPipeline&) = delete;
@@ -15,7 +23,10 @@ public:
     auto operator*() const noexcept -> const vk::raii::Pipeline& { return m_handle; }
     [[nodiscard]] auto GetHandle() -> const vk::raii::Pipeline& { return m_handle; }
 
+    [[nodiscard]] auto GetLayout() -> const vk::raii::PipelineLayout& { return m_layout; }
+
 private:
     vk::raii::Pipeline m_handle { nullptr };
+    vk::raii::PipelineLayout m_layout { nullptr };
 };
 }
