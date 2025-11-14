2f72cc8b95372f7e52e6e8657208a90e475414f4
Max <aaalllooollk@gmail.com>
2025-11-14 22:32:32 +0300

    Load models

--- skylabs/shaders/shaderSwapchain.frag.glsl
+++ skylabs/shaders/shaderSwapchain.frag.glsl
@@ -1,19 +1,10 @@
 #version 450
 
-layout(binding = 0) uniform sampler2D uLowRes;
+layout(binding = 0) uniform sampler2D uOut;
 
 layout(location = 0) in vec2 vUV;
 layout(location = 0) out vec4 outColor;
 
-void main()
-{
-    // семплим низкое разрешение
-    vec4 c = texture(uLowRes, vUV);
-
-    // Можно добавить nearest-neighbor для пикселей:
-    //   vec2 res = vec2(16.0, 16.0);
-    //   vec2 uv2 = floor(vUV * res) / res;
-    //   c = texture(uLowRes, uv2);
-
-    outColor = c;
+void main() {
+    outColor = texture(uOut, vUV);
 }
--- skylabs/shaders/shaderSwapchain.vert.glsl
+++ skylabs/shaders/shaderSwapchain.vert.glsl
@@ -3,15 +3,12 @@
 layout(location = 0) out vec2 vUV;
 
 vec2 positions[3] = vec2[](
-    vec2(-1.0, -3.0),
-    vec2( 3.0,  1.0),
-    vec2(-1.0,  1.0)
+    vec2(-1.0, -1.0),
+    vec2( 3.0, -1.0),
+    vec2(-1.0,  3.0)
 );
 
-void main()
-{
+void main() {
     gl_Position = vec4(positions[gl_VertexIndex], 0.0, 1.0);
-
-    // преобразуем в UV [0..1]
     vUV = gl_Position.xy * 0.5 + 0.5;
 }
