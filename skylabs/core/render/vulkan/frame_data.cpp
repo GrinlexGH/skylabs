16c3de4598ed9c24aac484f613817e6bf61cd2b5
Max <aaalllooollk@gmail.com>
2025-12-15 19:03:59 +0300

    Fix resizing

--- skylabs/core/render/vulkan/frame_data.cpp
+++ skylabs/core/render/vulkan/frame_data.cpp
@@ -1,10 +1,10 @@
 #include <skylabs/core/render/vulkan/frame_data.hpp>
 
 namespace Vulkan {
-CFrameData::CFrameData(const CContext& context) {
+CFrameData::CFrameData(const CContext& context) : m_context(&context) {
     //====================
     m_commandPool = vk::raii::CommandPool {
-        *context.GetDevice(),
+        *m_context->GetDevice(),
         {
             vk::CommandPoolCreateFlagBits::eResetCommandBuffer,
             context.GetDevice().GetGraphicsQueue().m_familyIndex
@@ -13,14 +13,14 @@ CFrameData::CFrameData(const CContext& context) {
 
     //====================
     m_commandBuffer = vk::raii::CommandBuffers {
-        *context.GetDevice(),
+        *m_context->GetDevice(),
         {m_commandPool, vk::CommandBufferLevel::ePrimary, 1}
     };
 
     //====================
-    m_fence = vk::raii::Fence { *context.GetDevice(), { vk::FenceCreateFlagBits::eSignaled } };
+    m_fence = vk::raii::Fence { *m_context->GetDevice(), { vk::FenceCreateFlagBits::eSignaled } };
 
     //====================
-    m_imageAvailableSemaphore = vk::raii::Semaphore { *context.GetDevice(), vk::SemaphoreCreateInfo {} };
+    m_imageAvailableSemaphore = vk::raii::Semaphore { *m_context->GetDevice(), vk::SemaphoreCreateInfo {} };
 }
 }
--- skylabs/core/render/vulkan/frame_data.hpp
+++ skylabs/core/render/vulkan/frame_data.hpp
@@ -17,7 +17,11 @@ public:
     [[nodiscard]] auto GetFence() const -> const vk::raii::Fence& { return m_fence; }
     [[nodiscard]] auto GetImageAvailableSemaphore() const -> const vk::raii::Semaphore& { return m_imageAvailableSemaphore; }
 
+    auto RecreateImageAvailableSemaphore() -> void { m_imageAvailableSemaphore = vk::raii::Semaphore { *m_context->GetDevice(), vk::SemaphoreCreateInfo {} }; }
+
 private:
+    const CContext* m_context = nullptr;
+
     vk::raii::CommandPool m_commandPool = nullptr;
     vk::raii::CommandBuffers m_commandBuffer = nullptr;
     vk::raii::Fence m_fence = nullptr;
