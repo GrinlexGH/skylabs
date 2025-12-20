6ddd3ff46802adbd300f65f4c667ce64c436015b
Max <aaalllooollk@gmail.com>
2025-12-20 18:48:37 +0300

    Fix fullscreen change on linux

--- skylabs/core/render/renderer.hpp
+++ skylabs/core/render/renderer.hpp
@@ -12,4 +12,10 @@ public:
     virtual ~IRenderer() = default;
 
     virtual auto Draw(glm::mat4, float) -> void = 0;
+
+    auto SetResizedState(bool isResized) -> void { m_isResized = isResized; }
+    auto GetResizedState() -> bool { return m_isResized; }
+
+private:
+    bool m_isResized = false;
 };
