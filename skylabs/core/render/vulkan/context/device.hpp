94a4dcbad97c2cfac4945755dfd0ab60535a0916
Max <aaalllooollk@gmail.com>
2026-01-02 00:30:56 +0300

    Add more convinient API

--- skylabs/core/render/vulkan/context/device.hpp
+++ skylabs/core/render/vulkan/context/device.hpp
@@ -28,6 +28,7 @@ public:
 
     [[nodiscard]] auto operator*() const noexcept -> const vk::raii::Device& { return m_handle; }
     [[nodiscard]] auto GetHandle() const noexcept -> const vk::raii::Device& { return m_handle; }
+    [[nodiscard]] auto operator->() const noexcept -> const vk::raii::Device* { return &m_handle; }
 
     [[nodiscard]] auto IsExtensionEnabled(const std::string_view name) const -> bool { return m_enabledExtensions.contains(name); }
 
