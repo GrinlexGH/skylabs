878f8b83b23986b5dbc6767efd553ce27ea61f04
Max <aaalllooollk@gmail.com>
2026-03-15 22:33:05 +0300

    Update Skylabs.cmake

--- cmake/Modules/Skylabs/Skylabs.cmake
+++ cmake/Modules/Skylabs/Skylabs.cmake
@@ -20,20 +20,13 @@ set(SKYLABS_VERSION_MINOR ${PROJECT_VERSION_MINOR})
 set(SKYLABS_VERSION_PATCH ${PROJECT_VERSION_PATCH})
 set(SKYLABS_VERSION_WORD "Melons")
 
-option(SKYLABS_TRY_LTO "Try to enable LTO" ON)
-
-# Enabling LTO
-if(SKYLABS_TRY_LTO)
-    include(CheckIPOSupported)
-    check_ipo_supported(RESULT LTO_AVAILABLE)
-    if(LTO_AVAILABLE)
-        message(STATUS "LTO has been enabled")
-    endif()
-endif()
+# CMake options
+include(CheckIPOSupported)
+check_ipo_supported(RESULT LTO_AVAILABLE)
+set(CMAKE_INTERPROCEDURAL_OPTIMIZATION ${LTO_AVAILABLE})
 
 set(CMAKE_POSITION_INDEPENDENT_CODE ON)
 
-# CMake settings
 set(CMAKE_CXX_STANDARD 23)
 set(CMAKE_CXX_STANDARD_REQUIRED ON)
 set(CMAKE_CXX_EXTENSIONS OFF)
@@ -51,12 +44,7 @@ set(CMAKE_VISIBILITY_INLINES_HIDDEN ON)
 # these libraries. It will then restore RUNPATH
 set(CMAKE_INSTALL_RPATH_USE_LINK_PATH ON)
 set(CMAKE_BUILD_WITH_INSTALL_RPATH ON)
-
-if(${CMAKE_SYSTEM_NAME} MATCHES "Darwin")
-    set(CMAKE_INSTALL_RPATH "@loader_path")
-else()
-    set(CMAKE_INSTALL_RPATH "\$ORIGIN")
-endif()
+set(CMAKE_INSTALL_RPATH "$<IF:$<PLATFORM_ID:Darwin>,@loader_path,\$ORIGIN>")
 
 set(CMAKE_PDB_OUTPUT_DIRECTORY ${SKYLABS_BIN_OUTPUT_DIRECTORY})
 set(CMAKE_COMPILE_PDB_OUTPUT_DIRECTORY ${SKYLABS_BIN_OUTPUT_DIRECTORY})
@@ -70,7 +58,7 @@ function(skylabs_configure_target target_name)
         return()
     endif()
 
-    get_property(aliased_target TARGET ${target_name} PROPERTY ALIASED_TARGET)
+    get_target_property(aliased_target ${target_name} ALIASED_TARGET)
     if(${aliased_target})
         return()
     endif()
@@ -79,7 +67,6 @@ function(skylabs_configure_target target_name)
     get_target_property(sources ${target_name} SOURCES)
     source_group(TREE "${CMAKE_CURRENT_SOURCE_DIR}" PREFIX "Source Files" FILES ${sources})
 
-    # Compile options
     get_target_property(target_type ${target_name} TYPE)
     set(allowed_types "STATIC_LIBRARY" "MODULE_LIBRARY" "SHARED_LIBRARY" "OBJECT_LIBRARY" "INTERFACE_LIBRARY" "EXECUTABLE")
     if(NOT(target_type IN_LIST allowed_types))
@@ -90,19 +77,18 @@ function(skylabs_configure_target target_name)
     set(IS_MSVC_LIKE $<OR:$<CXX_COMPILER_ID:MSVC>,$<STREQUAL:${CMAKE_CXX_COMPILER_FRONTEND_VARIANT},MSVC>>)
     set(SHOULD_USE_LTO $<AND:$<BOOL:${LTO_AVAILABLE}>,$<NOT:$<CONFIG:Debug>>>)
 
+    # Compile options
     target_compile_options(${target_name} PRIVATE
         $<$<OR:$<COMPILE_LANGUAGE:CXX>,$<COMPILE_LANGUAGE:C>>:
-            $<$<BOOL:${IS_GNU_LIKE}>:
+            $<${IS_GNU_LIKE}:
                 -Wall -Wextra -Wpedantic
-                $<$<AND:$<BOOL:${WIN32}>,$<CXX_COMPILER_ID:Clang>>:-fansi-escape-codes>
+                $<$<AND:$<PLATFORM_ID:Windows>,$<CXX_COMPILER_ID:Clang>>:-fansi-escape-codes>
             >
-            $<$<BOOL:${IS_MSVC_LIKE}>:
+            $<${IS_MSVC_LIKE}:
                 /utf-8 /Zc:__cplusplus /W4
-                $<$<BOOL:${SHOULD_USE_LTO}>:
+                $<${SHOULD_USE_LTO}:
                     /Gy /Gw
-                    $<$<CXX_COMPILER_ID:MSVC>:
-                        $<$<VERSION_GREATER_EQUAL:${CMAKE_CXX_COMPILER_VERSION},19.35>:/Zc:checkGwOdr>
-                    >
+                    $<$<AND:$<CXX_COMPILER_ID:MSVC>,$<VERSION_GREATER_EQUAL:${CMAKE_CXX_COMPILER_VERSION},19.35>>:/Zc:checkGwOdr>
                 >
             >
         >
@@ -110,26 +96,34 @@ function(skylabs_configure_target target_name)
 
     # Defines
     target_compile_definitions(${target_name} PRIVATE
-        $<$<CONFIG:Debug>:DEBUG> $<$<CONFIG:RelWithDebInfo>:DEBUG>
-        $<$<BOOL:${WIN32}>:PLATFORM_WINDOWS VK_USE_PLATFORM_WIN32_KHR UNICODE _UNICODE>
-        $<$<BOOL:${UNIX}>:PLATFORM_UNIX>
-        $<$<BOOL:${LINUX}>:PLATFORM_LINUX>
-        $<$<BOOL:${APPLE}>:PLATFORM_APPLE>
-        $<$<OR:$<C_COMPILER_ID:MSVC>,$<CXX_COMPILER_ID:MSVC>>:COMPILER_MSVC>
-        $<$<OR:$<C_COMPILER_ID:GNU>,$<CXX_COMPILER_ID:GNU>>:COMPILER_GCC>
-        $<$<OR:$<C_COMPILER_ID:Clang>,$<CXX_COMPILER_ID:Clang>>:COMPILER_Clang>
+        $<$<OR:$<CONFIG:Debug>,$<CONFIG:RelWithDebInfo>>:DEBUG>
+
+        $<$<PLATFORM_ID:Windows>:PLATFORM_WINDOWS VK_USE_PLATFORM_WIN32_KHR UNICODE _UNICODE>
+        $<$<PLATFORM_ID:Linux>:PLATFORM_LINUX>
+        $<$<PLATFORM_ID:Darwin>:PLATFORM_APPLE>
+
+        $<$<CXX_COMPILER_ID:MSVC>:COMPILER_MSVC>
+        $<$<CXX_COMPILER_ID:GNU>:COMPILER_GCC>
+        $<$<CXX_COMPILER_ID:Clang>:COMPILER_CLANG>
+        $<$<CXX_COMPILER_ID:AppleClang>:COMPILER_CLANG>
+
         $<$<EQUAL:${CMAKE_SIZEOF_VOID_P},8>:ARCH_64>
         $<$<EQUAL:${CMAKE_SIZEOF_VOID_P},4>:ARCH_32>
 
-        _CRT_SECURE_NO_WARNINGS NOMINMAX
+        _CRT_SECURE_NO_WARNINGS
+        NOMINMAX
+
         GLM_FORCE_DEPTH_ZERO_TO_ONE=1
         GLM_FORCE_RADIANS=1
         GLM_ENABLE_EXPERIMENTAL=1
+
         VULKAN_HPP_DISPATCH_LOADER_DYNAMIC=1
         VULKAN_HPP_TYPESAFE_CONVERSION=1
         VULKAN_HPP_USE_STD_EXPECTED=1
+
         VMA_STATIC_VULKAN_FUNCTIONS=0
         VMA_DYNAMIC_VULKAN_FUNCTIONS=0
+
         VP_USE_OBJECT=1
     )
 
