3a4a2716ca7469619e8d7e062be8461b9d3f4fd4
Max <aaalllooollk@gmail.com>
2026-04-16 17:53:16 +0300

    Cleaner camera

--- skylabs/core/camera.hpp
+++ skylabs/core/camera.hpp
@@ -5,79 +5,41 @@
 
 #include <algorithm>
 
-enum CameraMovement
-{
-    FORWARD,
-    BACKWARD,
-    LEFT,
-    RIGHT
-};
-
-constexpr float YAW = -90.0f;
-constexpr float PITCH = 0.0f;
-constexpr float SPEED = 1;
-constexpr float SENSITIVITY = 0.1f;
-constexpr float FOV = 90.0f;
-
 class CCamera
 {
 public:
-    glm::vec3 m_position;
-    glm::vec3 m_front;
-    glm::vec3 m_up;
-    glm::vec3 m_right;
-    glm::vec3 m_worldUp;
-
-    float m_yaw;
-    float m_pitch;
-
-    float m_movementSpeed;
-    float m_mouseSensitivity;
-    float m_fov;
+    enum class MoveDirection : std::int8_t
+    {
+        eForward,
+        eBackward,
+        eLeft,
+        eRight
+    };
 
     explicit CCamera(
         glm::vec3 position = glm::vec3(0.0f, 0.0f, 0.0f),
         glm::vec3 up = glm::vec3(0.0f, 0.0f, 1.0f),
         float yaw = YAW,
         float pitch = PITCH
-    ) : m_front(glm::vec3(1.0f, 0.0f, 0.0f)), m_movementSpeed(SPEED), m_mouseSensitivity(SENSITIVITY), m_fov(FOV) {
-        m_position = position;
-        m_worldUp = up;
-        m_yaw = yaw;
-        m_pitch = pitch;
+    ) : m_position(position), m_front(glm::vec3(1.0f, 0.0f, 0.0f)), m_worldUp(up), m_yaw(yaw), m_pitch(pitch) {
         UpdateCameraVectors();
     }
 
-    CCamera(
-        float posX,
-        float posY,
-        float posZ,
-        float upX,
-        float upY,
-        float upZ,
-        float yaw,
-        float pitch
-    ) : m_front(glm::vec3(1.0f, 0.0f, 0.0f)), m_movementSpeed(SPEED), m_mouseSensitivity(SENSITIVITY), m_fov(FOV) {
-        m_position = glm::vec3(posX, posY, posZ);
-        m_worldUp = glm::vec3(upX, upY, upZ);
-        m_yaw = yaw;
-        m_pitch = pitch;
-        UpdateCameraVectors();
-    }
-
-    glm::mat4 GetViewMatrix() const {
+    glm::mat4 ViewMatrix() const {
         return lookAt(m_position, m_position + m_front, m_up);
     }
 
-    void ProcessKeyboard(const CameraMovement direction, const float deltaTime) {
+    float Fov() const { return m_fov; }
+
+    void ProcessKeyboard(const MoveDirection direction, const float deltaTime) {
         const float velocity = m_movementSpeed * deltaTime;
-        if (direction == FORWARD)
+        if (direction == MoveDirection::eForward)
             m_position += m_front * velocity;
-        if (direction == BACKWARD)
+        if (direction == MoveDirection::eBackward)
             m_position -= m_front * velocity;
-        if (direction == LEFT)
+        if (direction == MoveDirection::eLeft)
             m_position -= m_right * velocity;
-        if (direction == RIGHT)
+        if (direction == MoveDirection::eRight)
             m_position += m_right * velocity;
     }
 
@@ -111,6 +73,25 @@ public:
     }
 
 private:
+    constexpr static const float YAW = -90.0f;
+    constexpr static const float PITCH = 0.0f;
+    constexpr static const float SPEED = 1;
+    constexpr static const float SENSITIVITY = 0.1f;
+    constexpr static const float FOV = 90.0f;
+
+    glm::vec3 m_position;
+    glm::vec3 m_front;
+    glm::vec3 m_up;
+    glm::vec3 m_right;
+    glm::vec3 m_worldUp;
+
+    float m_yaw = YAW;
+    float m_pitch = PITCH;
+
+    float m_movementSpeed = SPEED;
+    float m_mouseSensitivity = SENSITIVITY;
+    float m_fov = FOV;
+
     void UpdateCameraVectors() {
         glm::vec3 front;
         front.x = sin(glm::radians(m_yaw)) * cos(glm::radians(m_pitch));
