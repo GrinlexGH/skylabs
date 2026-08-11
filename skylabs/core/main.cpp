d887810ce7b061e6597268c21024e35f6e450ca6
Max <aaalllooollk@gmail.com>
2026-08-11 20:38:17 +0300

    Move console setup

--- skylabs/core/main.cpp
+++ skylabs/core/main.cpp
@@ -1,63 +1,7 @@
 #include <skylabs/core/launcher.hpp>
 #include <skylabs/public/dll_export.hpp>
 
-#ifdef PLATFORM_WINDOWS
-#include <skylabs/public/logging.hpp>
-
-namespace {
-std::string GetLastErrorMessage() {
-    wchar_t* errorMsg = nullptr;
-    FormatMessageW(
-        FORMAT_MESSAGE_ALLOCATE_BUFFER
-            | FORMAT_MESSAGE_FROM_SYSTEM
-            | FORMAT_MESSAGE_IGNORE_INSERTS,
-        nullptr,
-        GetLastError(),
-        MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
-        reinterpret_cast<LPWSTR>(&errorMsg),
-        0, nullptr
-    );
-
-    std::string finalMsg = boost::nowide::narrow(errorMsg);
-    LocalFree(errorMsg);
-    return finalMsg;
-}
-
-void EnableVTP() {
-    const HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
-    if (handle == INVALID_HANDLE_VALUE) {
-        Log::Error("Failed to get stdout handle: {}", GetLastErrorMessage());
-        return;
-    }
-
-    constexpr DWORD VTP_FLAGS = ENABLE_VIRTUAL_TERMINAL_PROCESSING | ENABLE_PROCESSED_OUTPUT;
-
-    DWORD originalMode = 0;
-    if (!GetConsoleMode(handle, &originalMode)) {
-        Log::Error("Failed to get console mode: {}", GetLastErrorMessage());
-        return;
-    }
-
-    if ((originalMode & VTP_FLAGS) == VTP_FLAGS) {
-        Log::Debug("Virtual terminal processing already set for STD_OUTPUT.");
-        return;
-    }
-
-    if (!SetConsoleMode(handle, originalMode | VTP_FLAGS)) {
-        Log::Error("Failed to set virtual terminal processing flags: {}", GetLastErrorMessage());
-    }
-}
-}
-#endif
-
 extern "C" DLL_EXPORT int CoreMain(int /*argc*/, char* /*argv*/[]) {
-#ifdef PLATFORM_WINDOWS
-    SetConsoleCP(CP_UTF8);
-    SetConsoleOutputCP(CP_UTF8);
-
-    EnableVTP();
-#endif
-
     CLauncher launcher;
     launcher.Run();
 
--- skylabs/launcher/main.c
+++ skylabs/launcher/main.c
@@ -12,7 +12,9 @@ typedef int (*main_t)(int argc, char* argv[]);
 #include <shellapi.h>
 
 static void PresentError(const wchar_t* msg) {
-    TASKDIALOGCONFIG tdc = { };
+    TASKDIALOGCONFIG tdc;
+    ZeroMemory(&tdc, sizeof(TASKDIALOGCONFIG));
+
     tdc.cbSize = sizeof(TASKDIALOGCONFIG);
     tdc.hwndParent = NULL;
     tdc.hInstance = NULL;
@@ -25,50 +27,50 @@ static void PresentError(const wchar_t* msg) {
     TaskDialogIndirect(&tdc, NULL, NULL, NULL);
 }
 
-// Max user message size is 864 (or 1024 - 128 - 32)
 static void PresentCError(const wchar_t* format, ...) {
-    wchar_t systemMessage[128];
+    wchar_t systemMessage[64];
     _wcserror_s(systemMessage, _countof(systemMessage), errno);
 
-    wchar_t contextMessage[1024 - 128 - 32];
+    wchar_t contextMessage[1024];
     va_list args;
     va_start(args, format);
     _vsnwprintf_s(contextMessage, _countof(contextMessage), _TRUNCATE, format, args);
     va_end(args);
 
-    wchar_t finalMessage[1024];
+    wchar_t finalMessage[_countof(systemMessage) + _countof(contextMessage) + 32];
     _snwprintf_s(
-        finalMessage, _countof(finalMessage), _TRUNCATE, L"%ls\n\nSystem reason: %ls", contextMessage, systemMessage
+        finalMessage, _countof(finalMessage), _TRUNCATE, L"%ls\n\nCRT error description: %ls", contextMessage, systemMessage
     );
 
     PresentError(finalMessage);
 }
 
-// Max user message size is 736 (or 1024 - 256 - 32)
-static void PresentSystemError(const wchar_t* format, ...) {
-    DWORD errorCode = GetLastError();
-
-    wchar_t systemMessage[256];
+static void GetSystemErrorMessage(wchar_t* message, const DWORD size, const DWORD errorCode) {
     DWORD msgLen = FormatMessageW(
         FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS | FORMAT_MESSAGE_MAX_WIDTH_MASK, NULL, errorCode,
-        MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), systemMessage, _countof(systemMessage), NULL
+        MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), message, size, NULL
     );
 
     if (msgLen == 0) {
-        _snwprintf_s(systemMessage, _countof(systemMessage), _TRUNCATE, L"Unknown Win32 Error (0x%08X)", errorCode);
+        _snwprintf_s(message, size, _TRUNCATE, L"Unknown Win32 Error (0x%08X)", errorCode);
     } else {
-        while (msgLen > 0 && (systemMessage[msgLen - 1] == L'\r' || systemMessage[msgLen - 1] == L'\n')) {
-            systemMessage[--msgLen] = L'\0';
+        while (msgLen > 0 && (message[msgLen - 1] == L'\r' || message[msgLen - 1] == L'\n')) {
+            message[--msgLen] = L'\0';
         }
     }
+}
 
-    wchar_t contextMessage[1024 - 256 - 32];
+static void PresentSystemError(const wchar_t* format, ...) {
+    wchar_t systemMessage[256];
+    GetSystemErrorMessage(systemMessage, _countof(systemMessage), GetLastError());
+
+    wchar_t contextMessage[1024];
     va_list args;
     va_start(args, format);
     _vsnwprintf_s(contextMessage, _countof(contextMessage), _TRUNCATE, format, args);
     va_end(args);
 
-    wchar_t finalMessage[1024];
+    wchar_t finalMessage[_countof(systemMessage) + _countof(contextMessage) + 32];
     _snwprintf_s(
         finalMessage, _countof(finalMessage), _TRUNCATE, L"%ls\n\nSystem reason: %ls", contextMessage, systemMessage
     );
@@ -114,7 +116,7 @@ static wchar_t* GetProgramPath(void) {
     exePath = SkMalloc(cap * sizeof(wchar_t));
 
     for (;;) {
-        DWORD size = GetModuleFileNameW(NULL, exePath, cap);
+        const DWORD size = GetModuleFileNameW(NULL, exePath, cap);
 
         if (size == 0) {
             PresentSystemError(L"Failed to get program path!");
@@ -202,11 +204,6 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLi
 
     // Remove filename
     wchar_t* lastSlash = wcsrchr(exePath, L'\\');
-    if (!lastSlash) {
-        PresentError(L"Invalid path format!");
-        goto cleanup;
-    }
-
     *lastSlash = L'\0';
 
     DWORD cap = (DWORD)(lastSlash - exePath) + _countof(LOAD_DIR) + _countof(LOAD_FILE) - 1;
@@ -259,8 +256,39 @@ cleanup:
     return ret;
 }
 
+static void EnableVTP() {
+    const HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+    if (handle == INVALID_HANDLE_VALUE) {
+        wchar_t systemMessage[256];
+        GetSystemErrorMessage(systemMessage, _countof(systemMessage), GetLastError());
+        printf("Failed to get stdout handle:\n%ls", systemMessage);
+        return;
+    }
+
+    const DWORD vtpFlags = ENABLE_VIRTUAL_TERMINAL_PROCESSING | ENABLE_PROCESSED_OUTPUT;
+
+    DWORD originalMode = 0;
+    if (!GetConsoleMode(handle, &originalMode)) {
+        wchar_t systemMessage[256];
+        GetSystemErrorMessage(systemMessage, _countof(systemMessage), GetLastError());
+        printf("Failed to get console mode:\n%ls", systemMessage);
+        return;
+    }
+
+    if (!SetConsoleMode(handle, originalMode | vtpFlags)) {
+        wchar_t systemMessage[256];
+        GetSystemErrorMessage(systemMessage, _countof(systemMessage), GetLastError());
+        printf("Failed to set virtual terminal processing flags:\n%ls", systemMessage);
+    }
+}
+
+/* Dummy main for console in debug */
 int main() {
-    /* Dummy main for console in debug */
+    // Setup windows console
+    SetConsoleCP(CP_UTF8);
+    SetConsoleOutputCP(CP_UTF8);
+    EnableVTP();
+
     return wWinMain(GetModuleHandleW(NULL), NULL, GetCommandLineW(), SW_SHOWNORMAL);
 }
 
