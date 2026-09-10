341cd7b2589d2d6f26a152683b87519138f335e3
Max <aaalllooollk@gmail.com>
2026-09-10 21:28:00 +0300

    Update vk-bootstrap

--- conanfile.py
+++ conanfile.py
@@ -30,7 +30,7 @@ class SkylabsRecipe(ConanFile):
         self.requires("sdl_mixer/3.2.4")
         self.requires("sdl_ttf/3.2.2")
         self.requires("tinyobjloader/2.0.0rc13")
-        self.requires("vk-bootstrap/1.4.361")
+        self.requires("vk-bootstrap/1.4.362")
         self.requires("vulkan-headers/1.4.362")
         self.requires("vulkan-memory-allocator/3.4.0")
         self.requires("vulkan-memory-allocator-hpp/3.4.0+2")
