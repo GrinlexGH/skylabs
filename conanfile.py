a021d796250765fa7730e75961dae81a022a8d4e
Max <aaalllooollk@gmail.com>
2026-09-11 20:32:07 +0300

    Update slang

--- conanfile.py
+++ conanfile.py
@@ -17,7 +17,7 @@ class SkylabsRecipe(ConanFile):
     }
 
     def build_requirements(self):
-        self.tool_requires("slang/2026.17")
+        self.tool_requires("slang/2026.17.1")
 
     def requirements(self):
         self.requires("boost/1.92.0")
